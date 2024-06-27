afb24b36030dfcb5bbcde01f1217c8d52c3d2a0a
Denis Saliakhov <denis.salyhov@saritasa.com>
2024-06-28 01:22:27 +0500

    Try to add Uploading to S3 storage

---
+++ SecretsSharingAPI/SecretsSharing.Infrastructure/S3StorageHandler.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.S3.Transfer;
+
+namespace SecretsSharing.Infrastructure;
+
+public class S3StorageHandler
+{
+    private const string bucketName = "sharing-secrets-drive";
+    private readonly TransferUtility transferUtility;
+
+    public S3StorageHandler(TransferUtility transferUtility)
+    {
+        this.transferUtility = transferUtility;
+    }
+
+    public Task Upload(Stream fileStream, string fileKey)
+    {
+        return transferUtility.UploadAsync(fileStream, bucketName, fileKey);
+    }
+}
---
+++ SecretsSharingAPI/SecretsSharing.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minio;
+using Minio.DataModel.Args;
+using SecretsSharing.Infrastructure;
+
+namespace SecretsSharing.WebApi.Controllers;
+
+[Route("api/files")]
+public class FilesController : ControllerBase
+{
+    private PostgresDbContext postgresDbContext;
+    private readonly S3StorageHandler s3StorageHandler;
+
+    public FilesController(PostgresDbContext postgresDbContext, S3StorageHandler s3StorageHandler)
+    {
+        this.postgresDbContext = postgresDbContext;
+        this.s3StorageHandler = s3StorageHandler;
+    }
+
+    [HttpPost("Upload")]
+    public async Task<ActionResult<string>> UploadFile(IFormFile formFile)
+    {
+        // var putObjectResponse = await minioClient.PutObjectAsync(new PutObjectArgs()
+        //     .WithBucket("secrets-sharing-drive")
+        //     .WithStreamData(formFile.OpenReadStream())
+        //     .WithObject(formFile.FileName)
+        //     .WithObjectSize(formFile.OpenReadStream().Length));
+
+        await s3StorageHandler.Upload(formFile.OpenReadStream(), formFile.FileName);
+
+        return Ok();
+    }
+
+    [HttpGet("users/{login}/password")]
+    public ActionResult<string> GetUserPassword(string login)
+    {
+        return Ok(postgresDbContext.Users.FirstOrDefault(user => user.Login == login));
+    }
+}
